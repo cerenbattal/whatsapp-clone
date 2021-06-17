da5df6f5fdea2c556a8d0efa490b3adaa18b678e
Ceren BATTAL <cebattal@netas.com.tr>
2021-06-17 22:55:18 +0300

    init project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Sidebar from './Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app__body">
+        {/** Sidebar */}
+        <Sidebar />
+        
+        {/** Chat */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDGk1zH0SQMwsyLPiON3a5iPFUEL5WvQ1E",
+    authDomain: "whatsapp-clone-145ee.firebaseapp.com",
+    projectId: "whatsapp-clone-145ee",
+    storageBucket: "whatsapp-clone-145ee.appspot.com",
+    messagingSenderId: "115627830461",
+    appId: "1:115627830461:web:1717514211b39cc68cdbd4",
+    measurementId: "G-3MXRMCNJKJ"
+  };
